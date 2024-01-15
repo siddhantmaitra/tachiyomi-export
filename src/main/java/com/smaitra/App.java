485fedc9a759e694de3db0512a3d67a9a9339a28
Siddhant Maitra <smaitra.dev@gmail.com>
2024-01-16 01:36:27 +0530

    init commit

---
+++ src/main/java/com/smaitra/App.java
@@ -0,0 +1,38 @@
+package com.smaitra;
+
+import com.google.protobuf.*;
+import com.google.protobuf.util.JsonFormat;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+// import com.smaitra.BackupOuterClass;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            // .tachibk OR .proto.gz OR .gz
+            FileInputStream fileInputStream = new FileInputStream("src/main/resources/backup_latest.tachibk");
+            GZIPInputStream gzipInputStream = new GZIPInputStream(fileInputStream);
+
+            
+            BackupOuterClass.Backup backup = BackupOuterClass.Backup.parseFrom(gzipInputStream);
+
+            // Convert protobuf object to JSON
+            String jsonFormat = JsonFormat.printer().print(backup);
+
+            FileOutputStream fileOutputStream = new FileOutputStream("src/main/resources/output02.json");
+            fileOutputStream.write(jsonFormat.getBytes());
+
+          
+            fileOutputStream.close();
+            gzipInputStream.close();
+            fileInputStream.close();
+
+            System.out.println("Successfully converted .proto.gz to .json");
+        } catch (IOException e) {
+            System.out.println("Error occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
