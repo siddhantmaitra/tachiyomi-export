8b53a7edb3fe6e23526c65cad009aa0eaf5ec26a
Siddhant Maitra <smaitra.dev@gmail.com>
2024-01-16 22:20:12 +0530

    chore: change file names

--- src/main/java/com/smaitra/App.java
+++ src/main/java/com/smaitra/App.java
@@ -12,7 +12,7 @@ public class App {
     public static void main(String[] args) {
         try {
             // .tachibk OR .proto.gz OR .gz
-            FileInputStream fileInputStream = new FileInputStream("src/main/resources/backup_latest.tachibk");
+            FileInputStream fileInputStream = new FileInputStream("src/main/resources/tachiyomi_test.tachibk");
             GZIPInputStream gzipInputStream = new GZIPInputStream(fileInputStream);
 
             
@@ -21,7 +21,7 @@ public class App {
             // Convert protobuf object to JSON
             String jsonFormat = JsonFormat.printer().print(backup);
 
-            FileOutputStream fileOutputStream = new FileOutputStream("src/main/resources/output02.json");
+            FileOutputStream fileOutputStream = new FileOutputStream("src/main/resources/tachiyomi_test.json");
             fileOutputStream.write(jsonFormat.getBytes());
 
           
